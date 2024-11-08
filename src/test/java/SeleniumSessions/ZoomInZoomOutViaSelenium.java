06095f6a55fc20ba8d93ff8e4049b497a24ac216
Saurabh <saurabh_d@outlook.com>
2024-11-08 18:55:00 +0530

    8/11 adding file without adding it into staged area

--- src/test/java/SeleniumSessions/ZoomInZoomOutViaSelenium.java
+++ src/test/java/SeleniumSessions/ZoomInZoomOutViaSelenium.java
@@ -1,6 +1,5 @@
 package SeleniumSessions;
 
-import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
